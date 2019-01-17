f24ebdac1d85ea9171c254cc066d41e1e4b259f3
meganhott <46502432+meganhott@users.noreply.github.com>
2019-01-17 00:11:48 -0500

    Update satellite.m

--- satellite.m
+++ satellite.m
@@ -10,39 +10,70 @@ M = 5.98*10^(24);
 R = 6.371*10^(6);
 
 h = (G*M*T^2/(4*pi^2))^(1/3)-R;
+v = (G*M/(h+R))^(1/2);
 
 if h>=0
     %change h to kilometers
     h = h/1000;
-    h = round(h);
     h1 = h; %used for plot at end
+    h = round(h);
 
+    %change v to km/h
+    v = v/1000;
+    v1 = v; %used for plot at end
+    v = round(v);
+    
     %output
-    formatSpec = "The satellite orbits at a height of %d km from Earth's surface";
-    sprintf(formatSpec, h)
+    formatSpec = "The satellite orbits at a height of %d km from Earth's surface with a speed of %d km/s";
+    sprintf(formatSpec, h, v)
 
     %plot
+    x0 = 0;
+    y0 = 0;
+    width = 1000;
+    height = 1000;
+    set(gcf, 'position', [x0, y0, width, height])
+    
+    ax1 = subplot(2,1,1);
+    ax2 = subplot(2,1,2);
+ 
+    %plot h vs T
     T = 0:648000;
-    x = T/3600;
+    x1 = T/3600;
     h = (G*M*T.^2/(4*pi.^2)).^(1/3)-R;
     y1 = h/1000;
 
-    plot(x, y1, 'b')
-    hold on
+    plot(ax1, x1, y1, 'b')
+    hold (ax1, 'on')
 
     y2 = zeros(1, 648001);
-    plot(x, y2, 'g')
-
-    title('Period vs Height Above Earth')
-    xlabel('Period (hours)')
-    ylabel('Height (km)')
+    plot(ax1, x1, y2, 'g')
 
     %point of intersection
-    plot(1.405, 0, '*r')
+    plot(ax1, 1.405, 0, '*r')
 
     %point of height from input period
-    plot(T1, h1, 'pk', 'MarkerSize', 12)
-
+    plot(ax1, T1, h1, 'pk', 'MarkerSize', 12)
+    
+    %plot h vs T
+    h = 0:50000000;
+    x2 = h/1000;
+    v = (G*M./(h+R)).^(1/2);
+    y3 = v/1000;
+    plot(ax2, x2, y3, 'b')
+    
+    %point of velocity from calculated height from input period
+    hold (ax2, 'on')
+    plot(ax2, h1, v1, 'pk', 'MarkerSize', 12)
+    
+    title(ax1, 'Height vs Period')
+    xlabel(ax1, 'Period (hours)')
+    ylabel(ax1, 'Height (km)')
+    
+    title(ax2, 'Velocity vs Height')
+    xlabel(ax2, 'Height (km)')
+    ylabel(ax2, 'Velocity (km/s)')
+    
 else
     %output
     formatSpec = "The period of the satellite is too low";
