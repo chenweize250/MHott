e0376a56022330503faf0b4474df8d2df3d96439
meganhott <46502432+meganhott@users.noreply.github.com>
2019-01-28 00:16:59 -0500

    Problem 5 Update

--- Problem5/problem5.py
+++ Problem5/problem5.py
@@ -6,15 +6,21 @@ Created on Wed Jan 23 20:08:36 2019
 @author: meganhott
 """
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import scipy.io as sio 
 
 "import data"
+"""
+absolute path
 xlsfile = "/Users/meganhott/Documents/GitHub/MHott/Problem5/GlobalCarbonBudget2018.xlsx"
-X = pd.read_excel(xlsfile, sheet_name = "Historical Budget", skiprows=114)
-
 txtfile = '/Users/meganhott/Documents/GitHub/MHott/Problem5/GlobalTempbyYear.txt'
+"""
+"relative path"
+xlsfile = 'GlobalCarbonBudget2018.xlsx'
+txtfile = 'GlobalTempbyYear.txt'
+X = pd.read_excel(xlsfile, sheet_name = "Historical Budget", skiprows=114)
 T = pd.read_table(txtfile, delimiter="\s+", header=None)
-
 
 "cumulative sum"
 industry = X.iloc[:, 1].values
@@ -25,16 +31,31 @@ totalsum = industrysum + landsum
 
 "plots"
 year = T.iloc[0:168, 0]
-plt.subplot(211)
-plt.plot(year, totalsum)
-plt.title("Cumulative Carbon Emissions vs Year")
-plt.xlabel("Cumulative Carbon Emissions")
-plt.ylabel("Average Global Temperature")
-
 temp = T.iloc[0:168, 1]
+plt.figure(figsize=(8,8))
+plt.subplot(211)
+ax = plt.gca()
+ax2 = ax.twinx()
+ax.plot(year, totalsum, label = 'Emissions')
+ax2.plot(year, temp, 'r')
+ax.plot(np.nan, 'r', label = 'Temperature')
+ax.legend(loc=0)
+
+plt.title("Cumulative Carbon Emissions vs Year")
+ax.grid()
+ax.set_xlabel("Year")
+ax.set_ylabel("Cumulative Carbon Emissions (10^12 kg)")
+y = "Average Global Temperature (" + u'\u00B0' + "C)"
+ax2.set_ylabel(y)
+
 plt.subplot(212)
 plt.plot(totalsum, temp)
+plt.grid(True)
+
 plt.title("Average Global Temperature vs Cumulative Carbon Emissions")
-plt.xlabel("Cumulative Carbon Emissions")
-plt.ylabel("Average Global Temperature")
-plt.subplots_adjust(hspace=1.6)
+plt.xlabel("Cumulative Carbon Emissions (10^12 kg)")
+plt.ylabel("Average Global Temperature (" + u'\u00B0' + "C)")
+plt.subplots_adjust(hspace=.5)
+
+"save to mat file"
+sio.savemat('problem5datapy.mat', {'Year':year, 'Average Global Temperature':temp, 'Cumulative Carbon Emissions':totalsum})
